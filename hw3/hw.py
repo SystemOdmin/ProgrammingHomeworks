50784766a620ec2d9dc035e058c8a2d106ea08fd
SystemOdmin <levkaz52@yandex.ru>
2016-11-13 23:04:39 +0300

    Исправлено дз №3

--- hw3/hw.py
+++ hw3/hw.py
@@ -2,10 +2,13 @@ a = int(input ("Введите a "))
 b = int(input ("Введите b "))
 c = int(input ("Введите c "))
 if a + b == c:
-    print ("a + b равно c")
+    print ("a + b = c")
 else:
-    print ("a + b не равно c")
-if a / b == c:
-    print ("a / b равно c")
+    print ("a + b ≠ c")
+if b == 0:
+    print ("a + b ≠ с, и вообще, на ноль делить нельзя!")
 else:
-    print ("a / b не равно c")
+    if a / b == c:
+        print ("a / b = c")
+    else:
+        print ("a / b ≠ c")
