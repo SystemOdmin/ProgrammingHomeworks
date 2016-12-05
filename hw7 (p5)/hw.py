373c756b10fc6ba57b576b1cd3b3b0c621149b8c
SystemOdmin <levkaz52@yandex.ru>
2016-12-05 20:07:50 +0300

    ДЗ №7

---
+++ hw7 (p5)/hw.py
@@ -0,0 +1,22 @@
+a = 0
+b = 0
+with open ('text.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        d.append(line)
+        line.replace('\ufeff', '')
+        line.replace('\n', '')
+        c = line.split()
+        for i in c:
+            if len(i) == 1:
+                a+=1
+            elif len(i) == 3:
+                b+=1
+if a == 0:
+    print('Слов длины 1 нет')
+if b == 0:
+    print('Слов длины 3 нет')
+else:
+    print(b/a) 
+        
+    
+    
