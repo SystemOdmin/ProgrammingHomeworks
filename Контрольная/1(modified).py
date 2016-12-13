440994401b92a90a8e74c21e093b45bd6205edb9
SystemOdmin <levkaz52@yandex.ru>
2016-12-13 14:01:34 +0300

    Delete 1(modified).py

--- Контрольная/1(modified).py
+++
@@ -1,7 +0,0 @@
-with open ('quotes.txt', 'r', encoding='utf-8') as f:
-    for line in f.readlines():
-        a = line.split()
-        if len(a) > 10:
-            print(' '.join(a))
-        else:
-            continue
