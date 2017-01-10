e6df9b521645143dff02a51c03378f7f86b9a526
SystemOdmin <levkaz52@yandex.ru>
2017-01-10 06:05:31 +0300

    hw10

--- hw10 (p6)/Generator.py
+++ hw10 (p6)/Generator.py
@@ -49,18 +49,36 @@ def direct_object():
             a.append(line.strip('\n'))
     return random.choice (a)
 
+def imperative_intransitive():
+    with open ('imperatives_intransitive.txt', 'r', encoding='utf-8') as f:
+        a = []
+        for line in f.readlines():
+            a.append(line.strip('\n'))
+    return random.choice (a)
+
+def imperative_transitive():
+    with open ('imperatives_transitive.txt', 'r', encoding='utf-8') as f:
+        a = []
+        for line in f.readlines():
+            a.append(line.strip('\n'))
+    return random.choice (a)
+
 def no():
     a = [' не ', ' ', ' ', ' ', ' ']
     return random.choice(a)
 
 def statement():
     a = adjective() + ' ' + noun() + no() + transitive() + ' ' + object_adjective() + ' ' + direct_object() + '.'
-    b = adjective() + ' ' + noun() + no() + transitive() + ' ' + object_adjective() + ' ' + direct_object() + '!'
-    c = adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + '.'
-    d = adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + '!'
-    x = [a, b, c, d]
+    b = adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + '.'
+    x = [a, b]
     return random.choice(x)
 
+def exclamation():
+    a = adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + '!'
+    b = adjective() + ' ' + noun() + no() + transitive() + ' ' + object_adjective() + ' ' + direct_object() + '!'
+    x = [a, b]
+    return random.choice(x)
+    
 def question():
     a = adverb() + ' ' + 'ли ' + adjective() + ' ' + noun() + ' ' + transitive() + ' ' + object_adjective() + ' ' + direct_object() + '?'
     b = adverb() + ' ' + 'ли ' + adjective() + ' ' + noun() + ' ' + intransitive() + '?'
@@ -69,18 +87,35 @@ def question():
     x = [a, b, c, d]
     return random.choice(x)
 
-def sentence():
-    x = statement()
-    y = statement()
-    z = question()
-    w = [x, y, z]
-    return random.choice (w)
+def condition():
+    a = 'Если ' + adjective() + ' ' + noun() + no() + transitive() + ' ' + object_adjective() + ' ' + direct_object() + ', то и ' + adjective() + ' ' + noun() + no() + transitive() + ' ' + object_adjective() + ' ' + direct_object() + '.'
+    b = 'Если ' + adjective() + ' ' + noun() + no() + transitive() + ' ' + object_adjective() + ' ' + direct_object() + ', то и ' + adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + '.'
+    c = 'Если ' + adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + ', то и ' + adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + '.'
+    d = 'Если ' + adjective() + ' ' + noun() + no() + intransitive() + ' ' + adverb() + ', то и ' + adjective() + ' ' + noun() + no() + transitive() + ' ' + object_adjective() + ' ' + direct_object() + '.'
+    x = [a, b, c, d]
+    return random.choice(x)
+
+def imperative():
+    a = (no() + imperative_intransitive() + ',' + adjective() + ' ' + noun() + '.').strip(' ')
+    b = (adjective() + ' ' + noun() + ',' + no() + imperative_transitive() + '.').strip(' ')
+    c = (no() + imperative_intransitive() + ', ' + adjective() + ' ' + noun() + '!').strip(' ')
+    d = (adjective() + ' ' + noun() + ',' + no() + imperative_transitive() + '!').strip(' ')
+    e = (no() + imperative_transitive() + ' ' + object_adjective() + ' ' + direct_object() + ', ' + adjective() + ' ' + noun() + '.').strip(' ')
+    f = (adjective() + ' ' + noun() + ',' + no() + imperative_transitive() + ' ' + object_adjective() + ' ' + direct_object() + '.').strip(' ')
+    g = (no() + imperative_transitive() + ' ' + object_adjective() + ' ' + direct_object() + ', ' + adjective() + ' ' + noun() + '!').strip(' ')
+    h = (adjective() + ' ' + noun() + ',' + no() + imperative_transitive() + ' ' + object_adjective() + ' ' + direct_object() + '!').strip(' ')
+    x = [a, b, c, d, e, f, g, h]
+    return random.choice(x)
 
 def generate():
-    a = []
-    for x in range(5):
-        a.append(sentence().capitalize())
-    print (' '.join(a))
+    a = statement().capitalize()
+    b = exclamation().capitalize()
+    c = question().capitalize()
+    d = condition()
+    e = imperative().capitalize()
+    w = [a, b, c, d, e]
+    random.shuffle (w)
+    print (' '.join(w))
     return
 
 generate()
