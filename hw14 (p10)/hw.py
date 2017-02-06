efb557c007a81d4c1b7317c09140e4909b9130ef
SystemOdmin <levkaz52@yandex.ru>
2017-02-06 23:55:58 +0300

    hw14

--- hw14 (p10)/hw.py
+++ hw14 (p10)/hw.py
@@ -3,4 +3,6 @@ import re
 a = input ('File name: ')
 with open (a, 'r', encoding='utf-8') as f:
     b = re.findall('Преподаватели<.+?>\n<.+?>\n<p>([0-9 ]+)', f.read())
-print (' '.join(b))
+with open ('prepody.txt', 'w', encoding='utf-8') as g:
+    c =' '.join(b)
+    g.write(c)
