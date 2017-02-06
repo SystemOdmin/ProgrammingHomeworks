cf70c2ab9c3a50de6919b055d294f32bba7a6390
SystemOdmin <levkaz52@yandex.ru>
2017-02-06 23:50:48 +0300

    hw14

--- hw14 (p10)/hw.py
+++ hw14 (p10)/hw.py
@@ -2,5 +2,5 @@ import re
 
 a = input ('File name: ')
 with open (a, 'r', encoding='utf-8') as f:
-    b = re.findall('Преподаватели</th>\n<td class="" style="padding:0.25em 0.5em; font-size:100%;">\n<p>([0-9 ]+)', f.read())
+    b = re.findall('Преподаватели<.+?>\n<.+?>\n<p>([0-9 ]+)', f.read())
 print (' '.join(b))
